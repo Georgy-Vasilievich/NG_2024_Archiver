a92dc9e292fbd968ca1c43a80da8ede5d8515b01
Heorhii Mykhailenko <georgy_vasilievich@ukr.net>
2024-09-02 22:57:48 +0300

    Add archive creation and opening

--- archiver.cpp
+++ archiver.cpp
@@ -6,10 +6,67 @@ Archiver::Archiver(QWidget *parent)
     , ui(new Ui::Archiver)
 {
     ui->setupUi(this);
+    connect(ui->b_create, &QPushButton::clicked, this, &Archiver::create);
+    connect(ui->b_open, &QPushButton::clicked, this, &Archiver::open);
+    connect(ui->b_add, &QPushButton::clicked, this, &Archiver::addFiles);
+    connect(ui->b_delete, &QPushButton::clicked, this, &Archiver::deleteFiles);
+    connect(ui->b_extract, &QPushButton::clicked, this, &Archiver::extractFiles);
 }
 
 Archiver::~Archiver()
 {
     delete ui;
+    closeArchive();
+}
+
+void Archiver::create()
+{
+    createOrOpen(true);
+}
+
+void Archiver::open()
+{
+    createOrOpen(false);
+}
+
+void Archiver::addFiles()
+{
+
+}
+
+void Archiver::deleteFiles()
+{
+
+}
+
+void Archiver::extractFiles()
+{
+
+}
+
+void Archiver::closeArchive()
+{
+    if (m_openedArchive != NULL) {
+        m_openedArchive->close();
+        delete m_openedArchive;
+    }
+}
+
+void Archiver::createOrOpen(bool create)
+{
+    QString fileName;
+    if (create)
+        fileName = QFileDialog::getSaveFileName(this, "Select file", "", "7-Zip archives (*.7z)");
+    else
+        fileName = QFileDialog::getOpenFileName(this, "Select file", "", "7-Zip archives (*.7z)");
+
+    if (fileName.isEmpty())
+        return;
+
+    closeArchive();
+
+    m_openedArchive = new K7Zip(fileName);
+
+    m_openedArchive->open(QIODevice::ReadWrite);
 }
 
--- archiver.h
+++ archiver.h
@@ -1,7 +1,9 @@
 #ifndef ARCHIVER_H
 #define ARCHIVER_H
 
+#include <QFileDialog>
 #include <QMainWindow>
+#include <k7zip.h>
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class Archiver; }
@@ -15,7 +17,17 @@ public:
     Archiver(QWidget *parent = nullptr);
     ~Archiver();
 
+private slots:
+    void create();
+    void open();
+    void addFiles();
+    void deleteFiles();
+    void extractFiles();
+
 private:
     Ui::Archiver *ui;
+    K7Zip* m_openedArchive = nullptr;
+    void closeArchive();
+    void createOrOpen(bool);
 };
 #endif // ARCHIVER_H
